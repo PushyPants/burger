774227ba5f277a7a66dc28362817019b6fd30e7d
Push <crashjohnson@gmail.com>
2018-08-07 11:50:48 -0500

    completed orm functions and tested. all functions working

--- config/orm.js
+++ config/orm.js
@@ -4,7 +4,6 @@ module.exports = {
     selectAll: function(table,calBack){
         connection.query(`SELECT * FROM ??`, [table],function (err, data){
             if (err) throw err;
-            console.log(data);
             callBack(data);
         })
     },
@@ -15,15 +14,14 @@ module.exports = {
         console.log(value)
         connection.query("INSERT INTO ?? (??) VALUE (?)", [table, col, value], function(err, data){
             if (err) throw err;
-            console.log(data);
             callBack(data);
         });
     },
 
     updateOne: function(table, col, colVal, where, whereEqual, callBack) {
-        connection.query('UPDATE (??) SET (??) = (?) WHERE (??) = (?)', [table, col, colVal, where, whereEqual], function(err, data){
-            console.log(data);
+        connection.query('UPDATE ?? SET ?? = ? WHERE ?? = ?', [table, col, colVal, where, whereEqual], function(err, data){
+            if (err) throw err;
             callBack(data);
-        }
+        });
     }
 }
--- server.js
+++ server.js
@@ -20,20 +20,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-//var routes = require("./controllers/catsController.js");
+Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
 
-//app.use(routes);
-
-let orm = require('./config/orm');
-
-
-
-orm.insertOne('burgers', 'burger_name', 'Newest Burger', 'burger_name', function(data){
-    console.log(data);
-})
-
-
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
